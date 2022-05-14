fff593d847d2653a0b7209ac60e0e0bc850ef9da
Vicwin13 <vnwimo13@gmail.com>
2022-05-14 13:45:56 +0100

    updated some files

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+let hamburger = document.getElementById("hamburger");
+let navLinks = document.getElementById("links");
+
+
+
+hamburger.addEventListener('click', () => {
+ console.log("my name is Victor") ;
+
+ navLinks.classList.toggle('toggle')
+})
